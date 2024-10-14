6eb6cc6673bc84728612c1e930381527f4d5fb27
ebluke <ethanluke.code@gmail.com>
2024-10-14 14:37:40 +1100

    skeleton started, pushing to work from laptop

---
+++ UserService/src/main/java/JOME/UserService/application/ShippingService.java
@@ -0,0 +1,76 @@
+package JOME.ShippingService.application;
+
+import JOME.ShippingService.domain.entity.Shipment;
+import JOME.ShippingService.domain.valueObject.ShippingStatus;
+import JOME.ShippingService.infrastructure.external.KafkaConsumerServiceOrder;
+import JOME.ShippingService.infrastructure.persistance.ShipmentRepository;
+import JOME.ShippingService.dto.ShipmentDTO;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ShippingService {
+
+    private final Shipment shipment;
+    private final ShipmentRepository shipmentRepository;
+
+
+    @Autowired
+    public ShippingService(Shipment _shipment, ShipmentRepository _shipmentRepository, KafkaConsumerServiceOrder kafkaConsumerServiceOrder) {
+        this.shipment = _shipment;
+        this.shipmentRepository = _shipmentRepository;
+    }
+
+    // API Expose
+
+    // GET
+    // Read a shipment by order ID
+    public Shipment getShipmentByOrderID(Long _orderID) {
+        return shipmentRepository.findByOrderID(_orderID);
+    }
+
+
+
+    // STATE UPDATE => mark as...
+
+    
+    public ShipmentDTO updateShipmentStatus(Long _orderID, String _status) {
+        Shipment shipment = shipmentRepository.findByOrderID(_orderID);
+        if (shipment == null) {
+            return null;
+        }
+        if (_status.equals("SHIPPED") && shipment.getShippingStatus().equals(Address.NOT_SHIPPED)) {
+            shipment.setToShipped();
+        } else if (_status.equals("DELIVERED") && shipment.getShippingStatus().equals(Address.SHIPPED)) {
+            shipment.setToDelivered();
+
+            // TODO: Notify the order service that the order has been delivered
+        } else {
+            return null;
+        }
+        shipmentRepository.save(shipment);
+        return new ShipmentDTO(shipment);
+    }
+
+
+    // Delete a shipment if it is not yet shipped
+    public void deleteShipment(Long _orderID) {
+        Shipment shipment = shipmentRepository.findByOrderID(_orderID);
+        if (shipment.getShippingStatus().equals(Address.NOT_SHIPPED)) {
+            shipmentRepository.delete(shipment);
+        } else {
+            throw new RuntimeException();
+        }
+    }
+
+
+
+
+    // handled by event
+
+
+
+
+
+}
---
+++ UserService/src/main/java/JOME/UserService/domain/entity/User.java
@@ -0,0 +1,106 @@
+package JOME.UserService.domain.entity;
+
+import jakarta.persistence.*;
+import java.util.List;
+import org.springframework.stereotype.Component;
+import JOME.UserService.domain.valueObject.Address;
+
+// make UserType, Cart, Order
+
+
+@Entity
+@Component
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String userID;
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String password;
+    private UserType userType;
+
+    private Cart cart;
+    private List<Order> orders;
+    
+    @Enumerated
+    private Address address;
+
+    // Shipment constructor to initialise a shippingStatus, order ID
+    public User (){}
+    public User(String userID, String name, String email, String phoneNumber, String password, UserType userType, Cart cart, List<Order> orders, Address address ) {
+        this.userID = userID;
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+        this.userType = userType;
+        this.cart = cart;
+        this.address = address;
+    }
+
+    // getters
+    public String getUserID(){
+        return this.userID;
+    }
+    public String getName(){
+        return this.name;
+    }
+    public String getEmail(){
+        return this.email;
+    }
+    public String getPhoneNumber(){
+        return this.phoneNumber;
+    }
+    public String getPassword(){
+        return this.password;
+    }
+    public UserType getUserType(){
+        return this.userType;
+    }
+    public Cart getCart(){
+        return this.cart;
+    }
+    public List<Order> getOrders(){
+        return this.orders;
+    }
+    public Address getAddress(){
+        return this.address;
+    }
+
+    // setters
+    public void setUserID(String userID){
+        this.userID = userID;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+    
+    public void setMobileNumber(String phoneNumber){
+        this.phoneNumber = phoneNumber;
+    }
+    public void setPassword(String password){
+        this.password = password;
+    }
+    public void setUserType(UserType userType){
+        this.userType = userType;
+    }
+    public void setCart(Cart cart){
+        this.cart = cart;
+    }
+    public void setOrders(List<Order> orders){
+        this.orders = orders;
+    }
+    public void setAddress(Address address){
+        this.address = address;
+    }
+
+    // update list / cart
+    public void updateOrders(Order newOrder){
+        this.orders.add(newOrder);
+    }
+
+}
